e72c38f9661b272600b1e2a13f418ab892f5f034
Epidemix-2020 <61560180+Epidemix-2020@users.noreply.github.com>
2021-02-02 21:29:46 -0600

    Add files via upload

---
+++ package.js
@@ -0,0 +1,25 @@
+{
+
+  "name": "Troller",
+
+  "description": "Trolls",
+
+  "version": "73.1.7",
+
+  "main": "bot.js",
+
+  "scripts": {
+
+    "start": "node bot.js"
+
+  },
+
+  "dependencies": {
+
+    "discord.js": "11.1.0",
+
+    "request": "2.81.0"
+
+  }
+
+}
